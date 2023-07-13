f871b2cdf749f51c590bbcce5068e1c1306ba260
Sujay Sakalkar <74325960+Sujay001coder@users.noreply.github.com>
2023-07-13 13:50:34 +0530

    Create Vending_tb.v

---
+++ Vending_tb.v
@@ -0,0 +1,32 @@
+module top_module;
+  //inputs
+  reg clk;
+    reg[2:0] in;
+  reg rst;
+  
+  //output
+  wire out;
+    wire[2:0] change;
+  
+vending_machine t(
+.clk(clk),
+.rst(rst),
+.in(in),
+.out(out),
+.change(change)
+);
+initial begin
+//initialise inputs
+    $monitor("On input=%0d,Vend=%0d,Change=%0d at time t=%0t",in,out,change,$time);
+    //$strobe ("On input=%0d,Vend=%0d,Change=%0d at time t=%0t",in,out,change,$time);
+    clk = 0;
+#5 rst = 1;
+
+#10 rst=0;
+    in=4;
+    #10 in=4;
+    //#10 in=2;
+   $finish;
+end
+always #5 clk = ~clk;
+endmodule
